47c66801640b5012ccb8c4505325aa6876ce49c4
Prasanth <prasanthkarthick3@gmail.com>
2025-02-17 15:24:16 +0530

    Currency fetch

--- src/Components/ProductScreen.js
+++ src/Components/ProductScreen.js
@@ -562,6 +562,7 @@ const ProductScreen = () => {
             setCurrence(correnc.data.value.currency)
           }
         }
+        localStorage.setItem('currence',currence);
       }
 
       fetchCurrence();
